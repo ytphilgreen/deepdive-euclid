fadd359a308049c990b20ffc492a0e251954cc1e
ali <ali@ali-PC.admin.ad.cnm.edu>
2017-06-14 10:53:27 -0600

    made the GCD access to the out

--- src/edu/cnm/deepdive/euclid/Gcd.java
+++ src/edu/cnm/deepdive/euclid/Gcd.java
@@ -3,11 +3,27 @@ package edu.cnm.deepdive.euclid;
 public class Gcd {
 
   public static void main(String[] args) {
-   int a = Integer.parseInt (args [0]);
-   int b = Integer.parseInt(args [1]);
-   System.out.println(a + " "+ b);
-   
+ 
+   try {
+     int a = Integer.parseInt (args [0]);
+     int b = Integer.parseInt(args [1]);
+    int result = gcd ( a, b);
+     
+     System.out.printf("gcd %d %d %d" ,a, b, result);     
+   }
+   catch (NumberFormatException ex) {
+     ex.printStackTrace();
+     System.out.println ("i need two numbers");
+     
+   }
+catch (ArrayIndexOutOfBoundsException ex) {
+  System.out.println("i need two numbers");
+  
+}
+  }
 
+  public static int gcd (int a, int b) {
+    return 0;
   }
 
 }
